400e56d506bac9619c95840a7ec156eaa9b80688
Paul Tiberghien <r1e6f@ugrad.cs.ubc.ca>
2024-07-04 19:31:16 +0200

    added skeleton of blog and interest section

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -1,9 +1,39 @@
-// somethign
+import React from "react";
+import ProjectCard from "../Components/ProjectCard";
 
-const Blog = () => {
+const projects = [
+  {
+    id: 1,
+    image: "/marathon.jpeg",
+    title: "Why Every Student Should Take a Personality Course ",
+    description: "coming soon:)",
+    link: "",
+    date: "",
+  },
+];
+
+const page = () => {
   return (
-    <h1>My Blog</h1>
-  )
-}
+    <>
+      <div className="text-center text-3xl font-bold mt-14">
+        A Few Writings ✍️
+      </div>
+      <div className="container mx-auto px-4 scale-75">
+        <div className="grid grid-cols-3 gap-4">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              image={project.image}
+              title={project.title}
+              description={project.description}
+              link={project.link}
+              date={project.date}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Blog
+export default page;
