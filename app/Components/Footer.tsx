9b3b1f760e2420bd0fe1cc00f02c32f542f2711a
paultibe <paultiberghien1@gmail.com>
2025-03-11 23:05:34 -0700

    add socials to footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,7 +1,44 @@
 const Footer = () => {
   return (
     <div className="container mx-auto py-16">
-      <div className="text-center">made with ❤️ and 🤹 !</div>
+      <div className="text-center">
+        made with ❤️ and 🤹 ! |{" "}
+        <a
+          href="https://linkedin.com/in/paultibe"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline text-blue-600"
+        >
+          LinkedIn
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/paultibe"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline text-blue-600"
+        >
+          Github
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://instagram.com/paul_tibe"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline text-blue-600"
+        >
+          Insta
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://www.youtube.com/@PaulTiberghien"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline text-blue-600"
+        >
+          YouTube
+        </a>
+      </div>
     </div>
   );
 };
